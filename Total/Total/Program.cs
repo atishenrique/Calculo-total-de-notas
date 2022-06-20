c5043c0e40770cdf988d9033d6ad13b92b23ac15
Atis Henrique <90425531+atishenrique@users.noreply.github.com>
2022-06-20 15:26:24 -0300

    Add files via upload

---
+++ Total/Total/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Media
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int semestre1 = 20;
+            int semestre2 = 20;
+            int semestre3 = 20;
+            double total = semestre1 + semestre2 + semestre3;
+
+            Console.WriteLine(total);
+        }
+    }
+}
